a9660bae7fd5f988449b76901642a88865a1b8b9
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-03 02:24:10 -0500

    Working WebSockets

--- lib/src/utils/http.dart
+++ lib/src/utils/http.dart
@@ -2,63 +2,74 @@ part of polymorphic.utils;
 
 class HttpHelper {
   static HttpClient client = new HttpClient();
-  
+
   static void forward(HttpRequest request, String newPath, String host, int port) {
     var response = request.response;
     client.open(request.method, host, port, newPath).then((req) {
       var completer = new Completer();
-      
+
       request.cookies.addAll(req.cookies);
       request.headers.forEach((name, value) {
         for (var v in value) {
           req.headers.add(name, v);
         }
       });
-      
+
       request.listen((data) {
         req.add(data);
       }).onDone(() {
         completer.complete(req.close());
       });
-      
+
       return completer.future;
     }).then((HttpClientResponse res) {
       // Special Handling for WebSockets
       if (res.statusCode == HttpStatus.SWITCHING_PROTOCOLS) {
-        Socket resSocket;
-        Socket responseSocket;
-        return res.detachSocket().then((socket) {
-          resSocket = socket;
-          
-          return response.detachSocket();
-        }).then((socket) {
-          responseSocket = socket;
-          return resSocket.pipe(socket);
-        }).then((_) {
-          responseSocket.destroy();
-          resSocket.destroy();
-        });
+        return proxyWebSocket(request, res);
       }
-      
+
       var completer = new Completer();
-      
+
       response.statusCode = res.statusCode;
-      
+
       res.headers.forEach((name, value) {
         for (var v in value) {
           response.headers.add(name, v);
         }
       });
-      
+
       response.cookies.addAll(res.cookies);
-      
+
       res.listen((data) {
         response.add(data);
       }).onDone(() {
         completer.complete(response.close());
       });
-      
+
       return completer.future;
     });
   }
+
+  static Future proxyWebSocket(HttpRequest request, HttpClientResponse response) {
+    WebSocket requestSocket;
+    WebSocket responseSocket;
+    
+    return WebSocketTransformer.upgrade(request).then((socket) {
+      requestSocket = socket;
+      return response.detachSocket();
+    }).then((socket) {
+      return new WebSocket.fromUpgradedSocket(socket, serverSide: false);
+    }).then((socket) {
+      responseSocket = socket;
+      
+      requestSocket.listen((data) {
+        responseSocket.add(data);
+      });
+      
+      responseSocket.listen((data) {
+        requestSocket.add(data);
+      });
+    });
+  }
 }
+
--- lib/utils.dart
+++ lib/utils.dart
@@ -1,6 +1,10 @@
 library polymorphic.utils;
 
 import "dart:async";
+import "dart:convert";
+import "dart:typed_data";
+import "package:crypto/crypto.dart";
+import "dart:math";
 import "dart:io";
 import "dart:isolate";
 
