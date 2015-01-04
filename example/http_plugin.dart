b3e97df96902483712847a3065a1e34f4715cf9b
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-03 22:24:30 -0500

    HTTP Plugin Example

---
+++ example/http_plugin.dart
@@ -0,0 +1,26 @@
+import "package:polymorphic_bot/api.dart";
+
+BotConnector bot;
+
+// Main Entry Point
+// A Plugin instance is passed in. Plugin Communication is handled with your plugin instance.
+void main(_, Plugin plugin) {
+  // Gets the Bot Connector for your plugin.
+  // The Bot Connector is what you use to communicate with the bot.
+  bot = plugin.getBot();
+  
+  // Starts an HTTP Server and routes it through Polymorphic's root HTTP Server.
+  // This then creates an HTTP Router.
+  plugin.createHttpRouter().then((router) {
+    // Route requests to / to this method.
+    router.addRoute("/", (request) {
+      request.response.writeln("Hello World");
+      request.response.close();
+    });
+  });
+  
+  // Handles Plugin Shutdowns
+  plugin.onShutdown(() {
+    // Do Something Important
+  });
+}
