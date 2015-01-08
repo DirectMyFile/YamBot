c71c797d4ace947a40e74fbbabc41b955c4eeb5f
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-08 18:33:01 -0500

    Tweak Build Script

--- tool/build.sh
+++ tool/build.sh
@@ -3,3 +3,5 @@
 [ ! -d out ] && mkdir out
 
 dart2js --enable-experimental-mirrors --categories=Server --output-type=dart bin/polymorphic.dart -o out/PolymorphicBot.dart -m
+dart2js --enable-experimental-mirrors --categories=Server --output-type=dart bin/polymorphic.dart -o out/PolymorphicBot_unminified.dart
+rm -rf out/*.deps
