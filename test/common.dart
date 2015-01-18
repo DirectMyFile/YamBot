560d445d2beff5b570b5b910bb6428c3534ea6d6
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-18 13:09:45 -0500

    Update Testing Library

--- test/common.dart
+++ test/common.dart
@@ -31,13 +31,13 @@ class Group {
 class SubGroup {
   final String name;
   final List<Type> subgroups;
-  
+
   const SubGroup(this.name, {this.subgroups});
 }
 
 class ExternalGroup {
   final String name;
-  
+
   const ExternalGroup(this.name);
 }
 
@@ -55,7 +55,7 @@ class Configure {
 
 class Mocker {
   final Type type;
-  
+
   const Mocker(this.type);
 }
 
@@ -63,7 +63,7 @@ void main() {
   findFunctionAnnotations(Configure).forEach((s) {
     s.function();
   });
-  
+
   scanLibrary(currentMirrorSystem().isolate.rootLibrary);
 }
 
@@ -105,12 +105,15 @@ void scanLibrary(LibraryMirror lib) {
       for (var f in tests) {
         setupTest(f);
       }
-      
+
       if (g.metadata.subgroups != null) {
         for (Type sub in g.metadata.subgroups) {
           var clazz = reflectClass(sub);
-          var s = clazz.metadata.firstWhere((it) => it.reflectee is SubGroup).reflectee;
-          var a = new ClassAnnotation()..metadata = s..mirror = clazz;
+          var s = clazz.metadata.firstWhere(
+              (it) => it.reflectee is SubGroup).reflectee;
+          var a = new ClassAnnotation()
+            ..metadata = s
+            ..mirror = clazz;
           scanGroup(a);
         }
       }
@@ -128,17 +131,19 @@ void scanLibrary(LibraryMirror lib) {
   findFunctionAnnotations(Start, lib: lib).forEach((s) {
     s.function();
   });
-  
+
   findFunctionAnnotations(Mocker, lib: lib).forEach((m) {
     var type = m.metadata.type;
     _mocks[type] = m.function;
   });
-  
-  var externals = lib.libraryDependencies.where((it) => it.isImport && it.metadata.any((it) => it.reflectee is ExternalGroup));
-  
+
+  var externals = lib.libraryDependencies.where((it) =>
+      it.isImport && it.metadata.any((it) => it.reflectee is ExternalGroup));
+
   for (var external in externals) {
-    ExternalGroup g = external.metadata.firstWhere((it) => it.reflectee is ExternalGroup).reflectee;
-    
+    ExternalGroup g = external.metadata.firstWhere(
+        (it) => it.reflectee is ExternalGroup).reflectee;
+
     group(g.name, () {
       scanLibrary(external.targetLibrary);
     });
@@ -155,10 +160,16 @@ class ClassAnnotation<T> {
   ClassMirror mirror;
 }
 
-List<ClassAnnotation> findClassesAnnotation(Type type, {LibraryMirror lib}) => (lib == null ? currentMirrorSystem().isolate.rootLibrary : lib).declarations.values.where((it) => it is ClassMirror && it.metadata.any((a) => reflectType(type).isAssignableTo(a.type))).map((it) {
+List<ClassAnnotation> findClassesAnnotation(Type type, {LibraryMirror lib}) =>
+    (lib == null ? currentMirrorSystem().isolate.rootLibrary :
+        lib).declarations.values
+    .where((it) => it is ClassMirror &&
+        it.metadata.any((a) => reflectType(type).isAssignableTo(a.type)))
+    .map((it) {
   return new ClassAnnotation()
-      ..metadata = (it.metadata.firstWhere((it) => reflectType(type).isAssignableTo(it.type)).reflectee)
-      ..mirror = it;
+    ..metadata = (it.metadata.firstWhere(
+        (it) => reflectType(type).isAssignableTo(it.type)).reflectee)
+    ..mirror = it;
 });
 
 Map<Type, Function> _mocks = {};
@@ -171,17 +182,21 @@ dynamic mockOf(Type type) {
   }
 }
 
-List<FunctionAnnotation> findFunctionAnnotations(Type type, {InstanceMirror instance, LibraryMirror lib}) {
+List<FunctionAnnotation> findFunctionAnnotations(Type type,
+    {InstanceMirror instance, LibraryMirror lib}) {
   var l = (lib == null ? currentMirrorSystem().isolate.rootLibrary : lib);
   var result = [];
   var t = reflectType(type);
-  var decl = instance != null ? instance.type.declarations.values : l.declarations.values;
+  var decl = instance != null ? instance.type.declarations.values :
+      l.declarations.values;
 
-  var mm = decl.where((it) => it is MethodMirror && it.metadata.any((f) => t.isAssignableTo(f.type)));
+  var mm = decl.where((it) =>
+      it is MethodMirror && it.metadata.any((f) => t.isAssignableTo(f.type)));
 
   for (var m in mm) {
     var a = new FunctionAnnotation();
-    a.metadata = m.metadata.firstWhere((it) => t.isAssignableTo(it.type)).reflectee;
+    a.metadata =
+        m.metadata.firstWhere((it) => t.isAssignableTo(it.type)).reflectee;
     a.function = ([input]) {
       var args = [];
       if (input != null) args.add(input);
