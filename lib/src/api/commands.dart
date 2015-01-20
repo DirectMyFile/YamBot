8e8767d6b69649455ef74a78b988a339f2f793ee
Kenneth Endfinger <kaendfinger@gmail.com>
2015-01-20 02:23:11 -0500

    More Documentation

--- lib/src/api/commands.dart
+++ lib/src/api/commands.dart
@@ -1,22 +1,61 @@
 part of polymorphic.api;
 
+/**
+ * A Command Event.
+ */
 class CommandEvent {
+  /**
+   * The Bot
+   */
   final BotConnector bot;
+  
+  /**
+   * Network
+   */
   final String network;
+  
+  /**
+   * Command
+   */
   final String command;
+  
+  /**
+   * Message
+   */
   final String message;
+  
+  /**
+   * User
+   */
   final String user;
+  
+  /**
+   * Channel
+   */
   final String channel;
+  
+  /**
+   * Command Arguments
+   */
   final List<String> args;
 
+  /**
+   * Sends [message] as a message to [channel] on [network].
+   */
   void reply(String message) {
     bot.sendMessage(network, channel, message);
   }
 
+  /**
+   * Calls [handle] if [user] has [permission].
+   */
   void require(String permission, void handle()) {
     bot.checkPermission((it) => handle(), network, channel, user, permission);
   }
 
+  /**
+   * Sends [message] as a notice to [channel] on [network].
+   */
   void replyNotice(String message) {
     bot.sendNotice(network, user, message);
   }
